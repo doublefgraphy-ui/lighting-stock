da6fcd5e5094e4d452bc7c2979c607da28dc9468
doublefgraphy-ui <doublefgraphy@gmail.com>
2026-08-01 16:24:16 +0900

    Delete images/config.js

--- images/config.js
+++
@@ -1,9 +0,0 @@
-window.PREORDER_CONFIG = {
-  endpoint: "https://script.google.com/macros/s/AKfycbxjdZQBlI7zKu8CQABheg0JPCcwZ8FBiiQ808VUeFQtMBOyMPSBvZfhgZ_u3b8g6sMW6Q/exec",
-
-  formKey: "lighting_stock_2026_inscale",
-
-  shopName: "lighting_stock_2026_inscale",
-  currency: "KRW",
-  source: "lighting-stock-github-pages"
-};
